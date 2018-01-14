4e2ab3d1cc43e650feb36cacec26a27304e0eecd
Unknown <patrickdugan04@gmail.com>
2018-01-14 10:25:25 -0800

    dis is good code

---
+++ src/Block.java
@@ -0,0 +1,145 @@
+import java.util.ArrayList;
+
+public class Block {
+	boolean isGoal; // to see if block is a goal
+	boolean isBlue; // to see what team the block is on
+	double x; // the blocks x coordinate
+	double y; // the blocks y coordinate
+	int yLineS;
+	int xLineS;
+	Block hit; // to remember the block you hit
+	// Blue is 0,0,200 Red is 200,0,0 Blue's Goal is 139,0,139 Red's Goal is 128,0,0
+	int incrX;// to determine which direction the block moves
+	int incrY;
+	double destX; // where the block is going
+	double destY;
+	int counter;
+	boolean moveing = false;
+	int win = 0;
+	int extraBlockTypes;
+
+	Block(boolean isGoalTemp, boolean isBlueTemp, int xTemp, int yTemp) {
+		isGoal = isGoalTemp;
+		isBlue = isBlueTemp;
+		x = xTemp;
+		y = yTemp;
+		destX = xTemp;
+		destY = yTemp;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+	double getX() {
+		return x;
+	}
+
+	int getincrX() {
+		return incrX;
+	}
+
+	int getincrY() {
+		return incrY;
+	}
+
+	void setX(double tempX) {
+		x = tempX;
+	}
+
+	void setY(double tempY) {
+		y = tempY;
+	}
+
+	void setMoveing(boolean temp) {
+		moveing = temp;
+	}
+
+	boolean getMoveing() {
+		return moveing;
+	}
+
+	double getY() {
+		return y;
+	}
+
+	boolean getIsGoal() {
+		return isGoal;
+	}
+
+	boolean getIsBlue() {
+		return isBlue;
+	}
+
+	double getDestX() {
+		return destX;
+	}
+
+	double getDestY() {
+		return destY;
+	}
+
+	void setHitNull() {
+		hit = null;
+	}
+
+	Block getHitValue() { // block that is getting hit
+		return hit;
+	}
+
+	void movementCalc(int incrXtemp, int incrYtemp, ArrayList Blocks) { // calculations for movement
+		incrX = incrXtemp;
+		incrY = incrYtemp;
+		moveing = true;
+		destX = x;
+		destY = y;
+		boolean done = true;
+		while (done) {
+			System.out.println("loop1");
+			if (isOverlap(Blocks)) {
+				destX = destX + incrX;
+				destY = destY + incrY;
+				System.out.println(destX + ",1 " + destY);
+			} else {
+				destX = destX + (incrX * -1); // if the destination is on a block it will flip the direction and
+												// then
+												// stop
+				destY = destY + (incrY * -1);
+				done = false;
+				System.out.println(destX + ",1 " + destY + "--------------------");
+				System.out.println(incrX + ",,, " + incrY);
+
+			}
+		}
+	}
+
+	boolean isOverlap(ArrayList Blocks) { // returns false if block touches other block
+		while (counter != Blocks.size()) {
+			System.out.println("loop2");
+			Block temp = (Block) Blocks.get(counter);
+			System.out.println(temp.getX() + " Block " + temp.getY() + "------------");
+			if ((temp.getX() == destX) && (temp.getY() == destY) && !(x == temp.getX() && y == temp.getY())) {
+				System.out.println("hit");
+				if (!temp.getIsGoal()) {
+					hit = temp;
+					return false;
+				}
+			}
+			if ((destX > 7) || (destX < 1) || (destY > 7) || (destY < 1)) {
+				return false;
+			}
+			counter++;
+		}
+		counter = 0;
+		return true;
+
+	}
+
+	void getHit(int directX, int directY, ArrayList Blocks) {
+		destX = destX + directX;
+		destY = destY + directY;
+		if (!isOverlap(Blocks)) {
+			destX = destX + (directX * -1);
+			destY = destY + (directY * -1);
+		}
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,235 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Main extends PApplet {
+	int sizeX = 600;
+	int sizeY = 600; // the x and y for the window
+	int lineSpaceingX = sizeX / 7; // How far apart the grid lines are
+	int lineSpaceingY = sizeY / 7;
+	double acceleration = .1; // how fast the blocks move
+	Block temp; // used to go through arrays
+	int counter = 0; // used for going through arrays
+	int secondaryCounter = 0;
+	boolean isBlueTurn = false; // to see hows turn it is
+	ArrayList<Block> Blocks = new ArrayList<Block>(); // the array list of all the Blocks
+	ArrayList<Block> Goals = new ArrayList<Block>(); // the array list of all the Goals
+	Block highlight = null; // to know what block is highlighted
+	Boolean isBlueWin = null;
+	PImage draw;
+	PImage background;
+	int backgroundNum;
+
+	public static void main(String[] args) {
+		PApplet.main("Main");
+	}
+
+	public void settings() {
+		size(sizeX, sizeY);
+	}
+
+	public void setup() {
+		System.out.println("What map would you like to play on 1,2,3,4");
+		Scanner scan = new Scanner(System.in);
+		backgroundNum = scan.nextInt();
+		// is Goal, is Blue, x, y
+		//doing blue team
+		addBlock(true,true, 4,7);
+		addBlock(false,true, 2,7);
+		addBlock(false,true, 3,7);
+		addBlock(false,true, 5,7);
+		addBlock(false,true, 6,7);
+		//doing red team
+		addBlock(true,false, 4,1);
+		addBlock(false,false, 2,1);
+		addBlock(false,false, 3,1);
+		addBlock(false,false, 5,1);
+		addBlock(false,false, 6,1);
+	
+	}
+
+	public void draw() {
+		clearSpace();
+		checkWin();
+		if (isBlueWin == null) {
+			drawBlocks();
+			advanceBlocks();
+			drawLines();
+		} else if (isBlueWin == false) {
+			fill(128, 0, 0);
+			rect(0, 0, sizeX, sizeY);
+		} else if (isBlueWin == true) {
+			fill(0, 0, 200);
+			rect(0, 0, sizeX, sizeY);
+		}
+
+	}
+
+	void drawLines() {
+		fill(0, 0, 0);
+		// drawing vertical lines
+		line(lineSpaceingX, 0, lineSpaceingX, sizeY);
+		line(lineSpaceingX * 2, 0, lineSpaceingX * 2, sizeY);
+		line(lineSpaceingX * 3, 0, lineSpaceingX * 3, sizeY);
+		line(lineSpaceingX * 4, 0, lineSpaceingX * 4, sizeY);
+		line(lineSpaceingX * 5, 0, lineSpaceingX * 5, sizeY);
+		line(lineSpaceingX * 6, 0, lineSpaceingX * 6, sizeY);
+		// drawing horizontal lines
+		line(0, lineSpaceingY, sizeX, lineSpaceingY);
+		line(0, lineSpaceingY * 2, sizeX, lineSpaceingY * 2);
+		line(0, lineSpaceingY * 3, sizeX, lineSpaceingY * 3);
+		line(0, lineSpaceingY * 4, sizeX, lineSpaceingY * 4);
+		line(0, lineSpaceingY * 5, sizeX, lineSpaceingY * 5);
+		line(0, lineSpaceingY * 6, sizeX, lineSpaceingY * 6);
+	}
+
+	void clearSpace() {
+		if (backgroundNum == 1) {
+			background = loadImage("sprites/background1.png");
+		} else if (backgroundNum == 2) {
+			background = loadImage("sprites/background2.png");
+		}
+		image(background, 0, 0, sizeX, sizeY);
+	}
+
+	void addBlock(boolean isGoal, boolean isBlue, int x, int y) {// so the Block gets put into the arrayList
+		if (isGoal) {
+			Goals.add(new Block(isGoal, isBlue, x, y));
+		}
+		Blocks.add(new Block(isGoal, isBlue, x, y));
+	}
+
+	void drawBlocks() {
+		while (counter != Blocks.size()) {
+			double tempX = Blocks.get(counter).getX(); // temporary x and y for the block to draw it
+			double tempY = Blocks.get(counter).getY();
+			boolean tempIsBlue = Blocks.get(counter).getIsBlue();
+			boolean tempIsGoal = Blocks.get(counter).getIsGoal();
+			if (tempIsBlue && tempIsGoal) {
+				draw = loadImage("sprites/blueGoal.png"); // blue's goal
+			} else if (tempIsBlue) {
+				draw = loadImage("sprites/blueBlock.png");
+			} else if (!tempIsBlue && tempIsGoal) {
+				draw = loadImage("sprites/redGoal.png");
+			} else if (!tempIsBlue) {
+				draw = loadImage("sprites/redBlock.png");
+			}
+			image(draw, (int) ((tempX - 1) * lineSpaceingX), (int) ((tempY - 1) * lineSpaceingY), lineSpaceingX,
+					lineSpaceingY);
+			if (Blocks.get(counter) == highlight) {
+				draw = loadImage("sprites/blueBorder.png");
+				image(draw, (int) ((tempX - 1) * lineSpaceingX), (int) ((tempY - 1) * lineSpaceingY), lineSpaceingX,
+						lineSpaceingY);
+			}
+
+			counter++; // Updating counter
+		}
+		counter = 0; // Reseting counter
+	}
+
+	Block checkBlockHigh() {// checks what block your mouse is over
+		while (counter != Blocks.size()) {
+			highlight = Blocks.get(counter);
+			if (((highlight.getX() - 1) * lineSpaceingX < mouseX) && (highlight.getX() * lineSpaceingX > mouseX)) {
+				if (((highlight.getY() - 1) * lineSpaceingY < mouseY) && (highlight.getY() * lineSpaceingY > mouseY)) {
+					if (highlight.getIsBlue() == isBlueTurn && !highlight.getIsGoal()) {
+						return highlight;
+					}
+				}
+
+			}
+			counter++;
+		}
+		highlight = null;
+		return null;
+
+	}
+
+	void advanceBlocks() { // move a block towards there destination
+		while (counter != Blocks.size()) {
+			temp = Blocks.get(counter);
+			if (temp.getDestX() > temp.getX()) {
+				temp.setX(temp.getX() + acceleration);
+			} else if (temp.getDestX() < temp.getX()) {
+				temp.setX(temp.getX() - acceleration);
+			} else if (temp.getDestY() > temp.getY()) {
+				temp.setY(temp.getY() + acceleration);
+			} else if (temp.getDestY() < temp.getY()) {
+				temp.setY(temp.getY() - acceleration);
+			} else if ((temp.getX() == temp.getDestX()) && (temp.getY() == temp.getDestY())) {
+				if (temp.getHitValue() != null) {
+					temp.getHitValue().getHit(temp.getincrX(), temp.getincrY(), Blocks);
+					temp.setHitNull();
+				}
+				temp.setMoveing(false);
+			}
+			System.out.println(
+					"dest " + temp.getDestX() + ", " + temp.getDestY() + " normal " + temp.getX() + " ," + temp.getY());
+			//
+			System.out.println(temp.getX() + ", " + (temp.getX() * 100) + .01 + ", "
+					+ Math.floor((temp.getX() * 100) + 1) + ", " + Math.floor((temp.getX() * 100) + .01) / 100);
+			// making the position to decimals
+			temp.setX(Math.floor((temp.getX() * 100) + .01) / 100);
+			temp.setY(Math.floor((temp.getY() * 100) + .01) / 100);
+			System.out.println(
+					"dest " + temp.getDestX() + ", " + temp.getDestY() + " normal " + temp.getX() + " ," + temp.getY());
+			counter++;
+		}
+		counter = 0;
+
+	}
+
+	public void mouseClicked() {
+		highlight = checkBlockHigh();
+	}
+
+	public void keyPressed() {
+		if (highlight != null) {
+			if (!highlight.getMoveing()) {
+				if (keyCode == UP) {
+					System.out.println("UP");
+					highlight.movementCalc(0, -1, Blocks);
+				} else if (keyCode == DOWN) {
+					System.out.println("DOWN");
+					highlight.movementCalc(0, 1, Blocks);
+				} else if (keyCode == LEFT) {
+					System.out.println("LEFT");
+					highlight.movementCalc(-1, 0, Blocks);
+				} else if (keyCode == RIGHT) {
+					System.out.println("RIGHT");
+					highlight.movementCalc(1, 0, Blocks);
+				}
+			}
+			highlight = null;
+			passTurn();
+		}
+	}
+
+	void passTurn() {
+		isBlueTurn = !isBlueTurn;
+	}
+
+	void checkWin() {
+		while (secondaryCounter != Goals.size()) {
+			while (counter != Blocks.size()) {
+				if (Blocks.get(counter).getX() == Goals.get(secondaryCounter).getX() // seeing is a block hit a goal
+						&& Blocks.get(counter).getY() == Goals.get(secondaryCounter).getY()) {
+					if (!Blocks.get(counter).getIsGoal()) {
+						if (Goals.get(secondaryCounter).getIsBlue()) {
+							isBlueWin = false;
+						} else if (!Goals.get(secondaryCounter).getIsBlue()) {
+							isBlueWin = true;
+						}
+					}
+				}
+				counter++;
+			}
+			counter = 0;
+			secondaryCounter++;
+		}
+		secondaryCounter = 0;
+	}
+
+}
